5017226e6536447d4f9b5774738915adb0b2ea9d
LucienChotard <chotardlucien@gmail.com>
2019-02-21 21:49:05 +0100

    clean

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 let body = document.querySelector("body")
 let vid = document.querySelector("#video")
 let trigger = document.querySelector("#video-trigger")
-
 trigger.addEventListener('click',
 function(){
   vid.play()
   body.style.overflowY = "hidden"
 });
-
 $('#modalvideo').on('hidden.bs.modal', function () {
   vid.pause()
   body.style.overflowY = "visible"
